1bff04e5df736326fdd4172e15039ef1ae9e6ecc
Mo <mo.ghorb@gmail.com>
2026-03-06 19:56:11 -0800

    refactor: remove commented-out request models

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 from models import Base
 
 
-# Using SQLite for simplicity
+
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./dispatcher.db")
 
 engine = create_engine(
